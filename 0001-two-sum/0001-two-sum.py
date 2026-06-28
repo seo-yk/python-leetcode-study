614638711514cbc9dd279d0105bff56e65933eff
Youkyung Seo <seoyk.dev@gmail.com>
2026-06-28 16:14:50 +0900

    Time: 1743 ms (16.75%), Space: 20 MB (66.89%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # time complexity: 최대 O(N)
+        
+        # 기준 1개 -> 하나씩 더해보기 -> index는 뒤로만 -> 찾으면 배열 추가 -> return
+        result = []
+
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    result.append(i)
+                    result.append(j)
+                    return result
+
+        
